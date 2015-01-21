60e6492429a698664fde63918689bcf7585b0acd
Carson Killbreath <kill4992@kettering.edu>
2015-01-20 21:05:21 -0500

    Some layout completed on race page.

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
-var users = require('./routes/users');
+var usersRoutes = require('./routes/users');
+var raceRoutes = require('./routes/race');
 
 var app = express();
 
@@ -23,7 +24,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/users', users);
+app.use('/users', usersRoutes);
+app.use('/race', raceRoutes);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
